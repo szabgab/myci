b2843c78daf4fe4db4e3e2bb0fe9c494ba84c99f
Gabor Szabo <gabor@szabgab.com>
2018-11-17 20:40:48 +0200

    be able to create multiple repositories in the test

--- test_repos.py
+++ test_repos.py
@@ -17,14 +17,14 @@ class TestRepo(object):
     def test_repo(self, tmpdir):
         temp_dir = str(tmpdir)
         print(temp_dir)
-        self.setup_repos(temp_dir)
+        self.setup_repos(temp_dir, 1)
 
         user_config = {
             'repos': [
                          {
                              'name' : 'main',
                              'type' : 'git',
-                             'url'  : self.repo1,
+                             'url'  : self.repo[0],
                          }
                      ]
         }
@@ -35,28 +35,28 @@ class TestRepo(object):
 
 
         _system("python check.py --server {} --config {} {}".format(self.server_file, self.config_file, debug))
-        assert os.path.exists( os.path.join(self.repos_parent, 'repo1') )
-        assert os.listdir( os.path.join(self.repos_parent, 'repo1/') ) == ['.git']
-        assert not os.path.exists(os.path.join(self.workdir, '1', 'repo1/'))
+        assert os.path.exists( os.path.join(self.repos_parent, 'repo0') )
+        assert os.listdir( os.path.join(self.repos_parent, 'repo0/') ) == ['.git']
+        assert not os.path.exists(os.path.join(self.workdir, '1', 'repo0/'))
 
 
     # git rev-parse HEAD
 
         # update the repository
-        with cwd(self.client1):
+        with cwd(self.client[0]):
             with open('README.txt', 'w') as fh:
                 fh.write("first line\n")
             _system("git add .")
             _system("git commit -m 'first' --author 'Foo Bar <foo@bar.com>'")
             _system("git push")
         _system("python check.py --server {} --config {} {}".format(self.server_file, self.config_file, debug))
-        assert os.listdir( os.path.join(self.repos_parent, 'repo1/') ) == ['README.txt', '.git']
-        assert os.path.exists(os.path.join(self.workdir, '1', 'repo1/'))
-        assert os.listdir(os.path.join(self.workdir, '1', 'repo1/')) == ['README.txt', '.git']
+        assert os.listdir( os.path.join(self.repos_parent, 'repo0/') ) == ['README.txt', '.git']
+        assert os.path.exists(os.path.join(self.workdir, '1', 'repo0/'))
+        assert os.listdir(os.path.join(self.workdir, '1', 'repo0/')) == ['README.txt', '.git']
         # check if the sha change was noticed
 
         # create a branch, see if the new branch is noticed
-        with cwd(self.client1):
+        with cwd(self.client[0]):
             with open('TODO', 'w') as fh:
                 fh.write("Some TODO text\n")
             _system("git checkout -b todo")
@@ -73,18 +73,18 @@ class TestRepo(object):
         _system("python check.py --server {} --config {} {}".format(self.server_file, self.config_file, debug))
 
         # first workdir did not change
-        assert os.path.exists(os.path.join(self.workdir, '1', 'repo1/'))
-        assert os.listdir(os.path.join(self.workdir, '1', 'repo1/')) == ['README.txt', '.git']
+        assert os.path.exists(os.path.join(self.workdir, '1', 'repo0/'))
+        assert os.listdir(os.path.join(self.workdir, '1', 'repo0/')) == ['README.txt', '.git']
 
         # second workdir has the new file as well
-        assert os.path.exists(os.path.join(self.workdir, '2', 'repo1/'))
-        assert os.listdir(os.path.join(self.workdir, '2', 'repo1/')) == ['MASTER', 'README.txt', '.git']
+        assert os.path.exists(os.path.join(self.workdir, '2', 'repo0/'))
+        assert os.listdir(os.path.join(self.workdir, '2', 'repo0/')) == ['MASTER', 'README.txt', '.git']
 
         # second workdir has the new file as well
-        assert os.path.exists(os.path.join(self.workdir, '3', 'repo1/'))
-        assert os.listdir(os.path.join(self.workdir, '3', 'repo1/')) == ['TODO', 'README.txt', '.git']
+        assert os.path.exists(os.path.join(self.workdir, '3', 'repo0/'))
+        assert os.listdir(os.path.join(self.workdir, '3', 'repo0/')) == ['TODO', 'README.txt', '.git']
 
-    def setup_repos(self, temp_dir):
+    def setup_repos(self, temp_dir, count):
         remote_repos = os.path.join(temp_dir, 'remote_repos')  # bare repos
         client_dir = os.path.join(temp_dir, 'client')  # workspace of users
         root = os.path.join(temp_dir, 'server')
@@ -92,8 +92,12 @@ class TestRepo(object):
         self.config_file = os.path.join(root, 'config.yml')  # this might be in a repository
         self.repos_parent = os.path.join(root, 'repos_parent')  # here is where we'll clone repos
         self.workdir = os.path.join(root, 'workdir')
-        self.repo1 = os.path.join(remote_repos, 'repo1')
-        self.client1 = os.path.join(client_dir, 'repo1')
+
+        self.repo = []
+        self.client = []
+        for i in range(count):
+            self.repo.append(os.path.join(remote_repos, 'repo' + str(i)))
+            self.client.append(os.path.join(client_dir, 'repo' + str(i)))
         os.mkdir(remote_repos)
         os.mkdir(client_dir)
         os.mkdir(root)
@@ -106,9 +110,11 @@ class TestRepo(object):
         }
         with open(self.server_file, 'w') as fh:
             fh.write(yaml.dump(server_config, explicit_start=True, default_flow_style=False))
+
         # in a subdirectory crete a git repository
-        os.mkdir(self.repo1)
-        with cwd(self.repo1):
-            _system("git init --bare")
-        with cwd(client_dir):
-            _system("git clone " + self.repo1)
+        for i in range(count):
+            os.mkdir(self.repo[i])
+            with cwd(self.repo[i]):
+                _system("git init --bare")
+            with cwd(client_dir):
+                _system("git clone " + self.repo[i])
