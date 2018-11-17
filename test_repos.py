40699f52ab7545d7338240c94ea67f504c2a1a44
Gabor Szabo <gabor@szabgab.com>
2018-11-17 20:32:05 +0200

    extract the setup_repos method

--- test_repos.py
+++ test_repos.py
@@ -17,34 +17,7 @@ class TestRepo(object):
     def test_repo(self, tmpdir):
         temp_dir = str(tmpdir)
         print(temp_dir)
-
-        remote_repos = os.path.join(str(tmpdir), 'remote_repos') # bare repos
-        client_dir   = os.path.join(str(tmpdir), 'client')       # workspace of users
-
-        root         = os.path.join(str(tmpdir), 'server')
-        self.server_file  = os.path.join(root, 'server.yml')
-        self.config_file  = os.path.join(root, 'config.yml')  # this might be in a repository
-
-        self.repos_parent  = os.path.join(root, 'repos_parent')  # here is where we'll clone repos
-        self.workdir       = os.path.join(root, 'workdir')
-
-        self.repo1         = os.path.join(remote_repos, 'repo1')
-        self.client1       = os.path.join(client_dir, 'repo1')
-
-        os.mkdir(remote_repos)
-        os.mkdir(client_dir)
-
-        os.mkdir(root)
-        os.mkdir(self.repos_parent)
-        os.mkdir(self.workdir)
-
-        # create config files
-        server_config = {
-            'root':    self.repos_parent,
-            'workdir': self.workdir,
-        }
-        with open(self.server_file, 'w') as fh:
-            fh.write(yaml.dump(server_config, explicit_start=True,  default_flow_style=False))
+        self.setup_repos(temp_dir)
 
         user_config = {
             'repos': [
@@ -59,12 +32,6 @@ class TestRepo(object):
             fh.write(yaml.dump(user_config, explicit_start=True,  default_flow_style=False))
 
 
-        # in a subdirectory crete a git repository
-        os.mkdir(self.repo1)
-        with cwd(self.repo1):
-            _system("git init --bare")
-        with cwd(client_dir):
-            _system("git clone " + self.repo1)
 
 
         _system("python check.py --server {} --config {} {}".format(self.server_file, self.config_file, debug))
@@ -116,3 +83,32 @@ class TestRepo(object):
         # second workdir has the new file as well
         assert os.path.exists(os.path.join(self.workdir, '3', 'repo1/'))
         assert os.listdir(os.path.join(self.workdir, '3', 'repo1/')) == ['TODO', 'README.txt', '.git']
+
+    def setup_repos(self, temp_dir):
+        remote_repos = os.path.join(temp_dir, 'remote_repos')  # bare repos
+        client_dir = os.path.join(temp_dir, 'client')  # workspace of users
+        root = os.path.join(temp_dir, 'server')
+        self.server_file = os.path.join(root, 'server.yml')
+        self.config_file = os.path.join(root, 'config.yml')  # this might be in a repository
+        self.repos_parent = os.path.join(root, 'repos_parent')  # here is where we'll clone repos
+        self.workdir = os.path.join(root, 'workdir')
+        self.repo1 = os.path.join(remote_repos, 'repo1')
+        self.client1 = os.path.join(client_dir, 'repo1')
+        os.mkdir(remote_repos)
+        os.mkdir(client_dir)
+        os.mkdir(root)
+        os.mkdir(self.repos_parent)
+        os.mkdir(self.workdir)
+        # create config files
+        server_config = {
+            'root': self.repos_parent,
+            'workdir': self.workdir,
+        }
+        with open(self.server_file, 'w') as fh:
+            fh.write(yaml.dump(server_config, explicit_start=True, default_flow_style=False))
+        # in a subdirectory crete a git repository
+        os.mkdir(self.repo1)
+        with cwd(self.repo1):
+            _system("git init --bare")
+        with cwd(client_dir):
+            _system("git clone " + self.repo1)
